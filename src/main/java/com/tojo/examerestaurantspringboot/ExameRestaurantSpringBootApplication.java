d0a0243fccc53e873aa0a8e71ffab87eb333dbee
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-04-06 17:54:34 +0300

    feat: initiating project

---
+++ src/main/java/com/tojo/examerestaurantspringboot/ExameRestaurantSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tojo.examerestaurantspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameRestaurantSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExameRestaurantSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tojo/examerestaurantspringboot/ExameRestaurantSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tojo.examerestaurantspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameRestaurantSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
